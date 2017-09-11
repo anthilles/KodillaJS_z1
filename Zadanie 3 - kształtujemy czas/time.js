2a1d4c592ea1fddd65fe53438bb401462a7c1fcc
anthilles <anthilles@outlook.com>
2017-09-11 14:27:42 +0200

    Zadanie - Kształtujemy czas

---
+++ Zadanie 3 - kształtujemy czas/time.js
@@ -0,0 +1,10 @@
+var os = require('os');
+
+function time(){
+    var uptime = os.uptime();
+    var h = Math.floor(uptime/3600) + 'godz ';
+    var m = Math.floor(uptime%3600/60) + 'min ';
+    var s = Math.floor(uptime%3600%60) + 'sek ';
+    console.log(h + m + s);
+}
+exports.TimeModul = time;
